b74c74826367bbeb9837dc1ed58a1b9945dc5a8e
mikidov <miki.dovlekaev2510@gmail.com>
2025-03-29 14:22:43 +0300

    Added basic explanation and structure of what the parseRegex function should do

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 // Declartions of functions
 int main();
+bool parseRegex(char *regex, char *str);
+bool isLetter(char c);
 
+// Function implementations
+bool parseRegex(char *regex, char *str)
+{
+    // parse regex (will currently suppert * and . on english characters)
+    // the parsing will use a switch case on each char against the currently supported tokens
+    // if it doesn't find a token (* or .) then it will add it to the current string literal
+    // when encountering '.' the program should just go to the next character
+    // when encountering '*' the program should look at the last character in the original string, and then pass it through a function to search as many occouarances of that
+    // the function should be aware that '.' is any character and so will always return true if it got it
+    
+    // String literal for matching anything with the string, let's say current string literals are 50 chars max as to not waste memeory resizing the string
+    // will need to be fixed\optimized later
+
+    return false;
+}
 
 int main()
 {
